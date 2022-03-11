370ad55b1d3e12c48dff205be72d54259f53a922
Sebjan1997 <joseph27sebastian@gmail.com>
2022-03-11 15:10:32 +0530

    intialization of setup

---
+++ Personal-Project-Management-Tool/src/main/java/com/seb/projects/Personal/Project/Management/Tool/PersonalProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.seb.projects.Personal.Project.Management.Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
+	}
+
+}
---
+++ Personal-Project-Management-Tool/src/test/java/com/seb/projects/Personal/Project/Management/Tool/PersonalProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb.projects.Personal.Project.Management.Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
